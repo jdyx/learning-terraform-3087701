48b930df818353e3c8bb683eddf605d855cbf204
jdyx <jdyandell9@gmail.com>
2025-06-28 15:23:44 +0100

    Use different version

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner]
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -20,14 +21,16 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
     Terraform = "true"
     Environment = var.environment.name
   }
 }
+
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
@@ -48,7 +51,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}-blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
@@ -58,7 +61,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -74,20 +77,18 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = var.environment.name
+    Environment = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name    = "${var.environment.name}-blog"
+  version = "4.13.0"
 
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
