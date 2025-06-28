733da7febb70d42c037558b0bec4e4f06ebc96cd
jdyx <jdyandell9@gmail.com>
2025-06-28 15:18:59 +0100

    Update name of ASG

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "${var.environment.name}-blog"
+  name = "blog"
 
   min_size            = var.asg_min_size
   max_size            = var.asg_max_size
